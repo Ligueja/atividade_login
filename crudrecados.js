0d8cbde3bacffe8200525058c6137eab7d9dd422
Luiz Henrique Coelho <127800714+Ligueja@users.noreply.github.com>
2023-12-04 17:52:18 -0300

    iniciando projeto login

---
+++ crudrecados.js
@@ -0,0 +1,37 @@
+const idUsuario = "ab33a6f0-8724-49df-acfa-22602e22e40d";
+
+const instance = axios.create({
+    baseURL: "http://localhost:8080",
+    // headers: {'X-Custom-Header': 'foobar'}
+  });
+
+  function cadastrar(event) {
+    event.preventDefault();
+    const titulo = event.srcElement.titulo.value;
+    const descricao = event.srcElement.descricao.value;
+  
+    instance.post(`/usuarios/${idUsuario}/recados`, {
+        titulo,
+        descricao,
+      })
+      // a função then é executada logo após o final da requisição
+      .then(function (response) {
+        console.log(response);
+      })
+      //   a função catch é executada casa ocorra qualquer tipo de erro na requisição
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
+  //   try {
+//     const response = instance.post(`/usuario/${idUsuario}/recados/`, {
+//       titulo,
+//       descricao
+//     })
+//     carregarRecados()
+//     console.log(response)
+//   } catch (error) {
+//     console.log(error)
+//   }
---
+++ login.js
@@ -0,0 +1,29 @@
+const idUsuario = "ab33a6f0-8724-49df-acfa-22602e22e40d";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8080",
+  // headers: {'X-Custom-Header': 'foobar'}
+});
+
+const formulario = document.getElementById("form");
+
+function logar(event) {
+  event.preventDefault();
+  const email = event.srcElement.email.value;
+  const senha = event.srcElement.senha.value;
+
+  instance
+    .post(`/usuarios/login`, {
+      email,
+      senha,
+    })
+    // a função then é executada logo após o final da requisição
+    .then(function (response) {
+      console.log(response);
+      alert("Usuário logado com sucesso!");
+    })
+    //   a função catch é executada casa ocorra qualquer tipo de erro na requisição
+    .catch(function (error) {
+      console.log(error);
+    });
+}
